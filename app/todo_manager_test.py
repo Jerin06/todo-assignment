da682657a501d05da8534dafdfa3e5a3bff33b94
Jerin M <mechjerin06@gmail.com>
2022-03-04 16:13:51 +0530

    unit test for todo manager

---
+++ app/todo_manager_test.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+class TodoManagerTest(unittest.TestCase):
+    def test_add_task(self):
+        todo_manager = TodoManager()
+        todo_manager.add_task("568", "The first task details everything")
+        task = todo_manager.get_task_by_id("568")
+        self.assertEqual("The first task details everything", task)
